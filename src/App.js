870e8b54a80910b0834215a1d158cc9b8ad09608
Richard McGirt <richardmcgirt@Richards-Mini.attlocal.net>
2023-09-13 12:40:53 -0400

    moved CB and CF to Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+// Make sure to import the component we just built:
+import ColorBlock from "./Components/ColorBlock"
+import ColorForm from "./Components/ColorForm"
+import "./App.css"
 
 function App() {
+  let [colors, setColors] = useState([
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "indigo",
+    "violet",
+  ])
+
+  const addColor = (newColor) => {
+    setColors([...colors, newColor])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {colors.map((color, i) => (
+        <ColorBlock key={i} color={color} />
+      ))}
+      <ColorForm addColor={addColor} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
